1c650726993b2dc07bd5f4eb3f045cdf142fd156
Michael Fleming <michael.fleming@nih.gov>
2020-11-25 16:51:57 -0500

    updates for specificity of bucket names
    updates for specificity of bucket names

--- nci-seronet-lambdaDeployment-proc/lambdaDeployment_v2.js
+++ nci-seronet-lambdaDeployment-proc/lambdaDeployment_v2.js
@@ -1,11 +1,10 @@
-console.log('Loading function');
 var AWS = require('aws-sdk');
 var lambda = new AWS.Lambda();
 exports.handler = function(event, context) {
     key = event.Records[0].s3.object.key
     bucket = event.Records[0].s3.bucket.name
-    if (bucket == "nci-seronet-proc-layers-test") {
-        console.log('Create new shared layer');
+    if (bucket == "nci-seronet-proc-layers") {
+        console.log('Create new shared layer for Seronet proc functions');
 		var layerParams = {
 			CompatibleRuntimes: [ 
 				"python3.8"
@@ -14,7 +13,6 @@ exports.handler = function(event, context) {
 				S3Bucket: bucket, 
 				S3Key: key
 			}, 
-			Description: "My Python layer", 
 			LayerName: bucket, 
 			//LicenseInfo: "MIT"
 			};
@@ -26,37 +24,28 @@ exports.handler = function(event, context) {
 				};
 		lambda.publishLayerVersion(layerParams, function(err, layerData) {
 			if (err) console.log(err, err.stack);
-			//else return data.LayerVersionArn;
-			//else console.log(data.LayerVersionArn);
-			
-			else lambda.listFunctions(funcParams, function(err, data) {
+			else lambda.listFunctions(funcParams, function(err, funcData) {
 					if (err) console.log(err, err.stack);
-					else data.Functions.forEach(function(lambdaFunc) {
+					else funcData.Functions.forEach(function(lambdaFunc) {
 						if (lambdaFunc.FunctionName.includes("nci-seronet-proc")) {
-							console.log(lambdaFunc.FunctionName);
-							console.log("update to layer: " + layerData.LayerVersionArn);
+							console.log("Function Name: " + lambdaFunc.FunctionName);
+							console.log("Layer to Use: " + layerData.LayerVersionArn);
+							var params = {
+								FunctionName: lambdaFunc.FunctionName, 
+								Layers: [
+									layerData.LayerVersionArn,
+								],
+							};
+							lambda.updateFunctionConfiguration(params, function(err, data) {
+								if (err) console.log(err, err.stack); // an error occurred
+								else     console.log(data);
+								});
 							}
 						});
 					});
 			
 			});
 			
-		//console.log('Update all seronet functions to use the latest layer');
-		//var params = {
-			//MasterRegion: "us-east-1",
-			//FunctionVersion: "ALL",
-			//Marker: '',
-		//		MaxItems: 50
-		//		};
-		//lambda.listFunctions(params, function(err, data) {
-		//	if (err) console.log(err, err.stack);
-		//	else data.Functions.forEach(function(lambdaFunc) {
-		//		if (lambdaFunc.FunctionName.includes("nci-seronet-proc")) {
-		//			console.log(lambdaFunc.FunctionName);
-		//			console.log("update to layer: " + newLayer);
-		//			}
-		//		});
-		//	});
     } else {
         var functionName = bucket;
         console.log("uploaded to lambda function: " + functionName);
