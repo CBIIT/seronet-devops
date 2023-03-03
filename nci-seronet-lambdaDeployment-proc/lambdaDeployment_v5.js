54e3a86d717d5c64e1953148e8852340f182b518
Michael Fleming <michael.fleming@nih.gov>
2023-03-03 16:36:15 -0500

    updated for db layers

--- nci-seronet-lambdaDeployment-proc/lambdaDeployment_v5.js
+++ nci-seronet-lambdaDeployment-proc/lambdaDeployment_v5.js
@@ -5,7 +5,7 @@ var lambda = new AWS.Lambda();
 exports.handler = function(event, context) {
     key = event.Records[0].s3.object.key
     bucket = event.Records[0].s3.bucket.name
-    if (bucket.includes("nci-seronet-proc-layers")) {
+    if (bucket.includes("layers")) {
         console.log('Create new shared layer for Seronet proc functions');
 		var layerParams = {
 			CompatibleRuntimes: [ 
@@ -29,7 +29,23 @@ exports.handler = function(event, context) {
 			else lambda.listFunctions(funcParams, function(err, funcData) {
 					if (err) console.log(err, err.stack);
 					else funcData.Functions.forEach(function(lambdaFunc) {
-						if (lambdaFunc.FunctionName.includes("nci-seronet-proc") || lambdaFunc.FunctionName.includes("seronet-file-remover-sns")) {
+						if (bucket.includes("db-layers")) {
+							if (lambdaFunc.FunctionName.includes("nci-seronet-proc-sql-db-uploader")) {
+								console.log("Function Name: " + lambdaFunc.FunctionName);
+								console.log("Layer to Use: " + layerData.LayerVersionArn);
+								var params = {
+									FunctionName: lambdaFunc.FunctionName, 
+									Layers: [
+										layerData.LayerVersionArn,
+									],
+								};
+								lambda.updateFunctionConfiguration(params, function(err, data) {
+									if (err) console.log(err, err.stack); // an error occurred
+									else     console.log(data);
+									});
+							}
+						} else {
+							if (lambdaFunc.FunctionName.includes("nci-seronet-proc") || lambdaFunc.FunctionName.includes("seronet-file-remover-sns")) {
 							console.log("Function Name: " + lambdaFunc.FunctionName);
 							console.log("Layer to Use: " + layerData.LayerVersionArn);
 							var params = {
@@ -43,6 +59,7 @@ exports.handler = function(event, context) {
 								else     console.log(data);
 								});
 							}
+						}
 						});
 					});
 			
